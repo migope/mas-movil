4a43219e97ef3aba899fd596af4a24758635d7dc
migope <migue3080@gmail.com>
2020-12-20 18:25:21 +0100

    third-part: 3

--- airflow_dags/test.py
+++ airflow_dags/test.py
@@ -5,6 +5,8 @@ from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 
 
+NUMBER_DUMMY_TASKS = 5  # number of dummy task of the part 3
+
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
@@ -21,4 +23,20 @@ dag = DAG(
 start = DummyOperator(task_id='start', dag=dag)
 end = DummyOperator(task_id='end', dag=dag)
 
+
+# create the N dummy tasks
+dummy_tasks_pair = []
+dummy_tasks_odd = []
+
+for n in range(NUMBER_DUMMY_TASKS):
+    task = DummyOperator(task_id=f'task_{n}', dag=dag)
+    if (n % 2) == 0:
+        dummy_tasks_pair.append(task)
+    else:
+        dummy_tasks_odd.append(task)
+
+# create the dependencies
 start >> end
+
+for odd_task in dummy_tasks_odd:
+    odd_task >> dummy_tasks_pair
